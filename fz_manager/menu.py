b7a1e76f3cd2e51cc4bd70fce7c6134ea050903c
michelsciortino <michel.sciortino@outlook.com>
2021-12-28 16:11:34 +0100

    Changing menu dependency for cross-os compatibility

--- fz_manager/menu.py
+++ fz_manager/menu.py
@@ -1,64 +1,81 @@
 from typing import Callable
-from simple_term_menu import TerminalMenu
+import questionary
+import os
 
-class MenuEntry():
-    def __init__(self, name: str, callback: Callable = None, pre_selected: bool = False, ext_index : (int|str) = None):
+from questionary import Choice
+
+
+def cls():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+class MenuEntry:
+    def __init__(self, name: str, callback: Callable = None, pre_selected: bool = False, ext_index: (int | str) = None):
         self.name = name
         self.callback = callback
         self.pre_selected = pre_selected
         self.ext_index = ext_index
 
-class ActionMenu():
-    def __init__(self, title: str, entries: list[MenuEntry], exit: str|list[str] = None, clear_screen: bool = False):
+
+class ActionMenu:
+    def __init__(self, title: str, entries: list[MenuEntry], exit_entry: str | list[str] = None, clear_screen: bool = False):
         self.title = title
         self.entries = entries
         self.cls = clear_screen
-        self.ext = exit
+        self.ext = exit_entry
 
     def show(self) -> None:
         while True:
-            menu = TerminalMenu(
-                [e.name for e in self.entries],
-                clear_screen=self.cls,
-                title=self.title
-            )
-            choice = menu.show()
+            self.cls and cls()
+            choice = questionary.select(
+                self.title,
+                [Choice(e.name, e) for e in self.entries],
+                qmark='',
+                instruction=' '
+            ).ask()
+
             if choice is None:
-                return
-
-            entry = self.entries[choice]
-            entry.callback and entry.callback()
-            
-            if self.ext is str and self.ext==entry.name:
-                break
-            if self.ext is list and entry.name in self.ext:
                 break
 
-class SelectMenu():
+            choice.callback and choice.callback()
+
+            if isinstance(self.ext, str) and self.ext == choice.name:
+                break
+            if isinstance(self.ext, list) and choice.name in self.ext:
+                break
+
+
+class SelectMenu:
     def __init__(self, title: str, entries: list[MenuEntry], clear_screen: bool = False, multi_select: bool = False):
         self.title = title
         self.entries = entries
         self.multi = multi_select
         self.cls = clear_screen
 
-    def show(self) -> (MenuEntry | tuple[list[MenuEntry], list[MenuEntry], list[MenuEntry]]):
-        preselected = [i for i,e in enumerate(self.entries) if e.pre_selected]
-        menu = TerminalMenu(
-            [e.name for e in self.entries],
-            preselected_entries=preselected,
-            clear_screen=self.cls,
-            title=self.title,
-            multi_select=self.multi,
-            multi_select_select_on_accept=False
-        )
-        choice = menu.show()
+    def show(self) -> (MenuEntry | tuple[list[MenuEntry], list[MenuEntry], list[MenuEntry]] | tuple[None, None, None]):
+        self.cls and cls()
+
+        if not self.multi:
+            return questionary.select(
+                message=self.title,
+                choices=[Choice(e.name, e) for e in self.entries],
+                qmark='',
+                instruction=' '
+            ).ask()
+
+        choice = questionary.checkbox(
+            message=self.title,
+            choices=[Choice(e.name, e, checked=e.pre_selected) for e in self.entries],
+            qmark=''
+        ).ask()
+
         if choice is None:
-            return (None,None,None)
-        
+            return None, None, None
+
         if not self.multi:
             return self.entries[choice]
         else:
-            selected : list[MenuEntry]= []
+            selected: list[MenuEntry] = []
             added: list[MenuEntry] = []
             removed: list[MenuEntry] = []
             for i, entry in enumerate(self.entries):
@@ -68,4 +85,8 @@ class SelectMenu():
                         added.append(entry)
                 elif entry.pre_selected:
                     removed.append(entry)
-            return (selected,added,removed)
+            return selected, added, removed
+
+
+def show_message_menu(message):
+    return ActionMenu(message, [MenuEntry('Back')], 'Back').show()
