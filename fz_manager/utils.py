ad596906ccde8455703291f46c552d82736e93f1
michelsciortino <michel.sciortino@outlook.com>
2022-01-03 12:02:28 +0100

    New methods: rgb_to_hex, run_on_thread

--- fz_manager/utils.py
+++ fz_manager/utils.py
@@ -1,4 +1,7 @@
+import asyncio
 import os
+import threading
+import inspect
 
 
 class Term:
@@ -59,8 +62,52 @@ class Colors:
     BLUE = 30, 144, 255
     ORANGE = 255, 165, 0
 
+    @staticmethod
+    def rgb_to_hex(rgb: tuple[int, int, int]):
+        def clamp(x):
+            return max(0, min(x, 255))
+
+        return f'#{clamp(rgb[0]):02x}{clamp(rgb[1]):02x}{clamp(rgb[2]):02x}'
+
+    FACTORIO_FG_HEX = rgb_to_hex(FACTORIO_FG)
+    FACTORIO_BG_HEX = rgb_to_hex(FACTORIO_BG)
+
 
 class String:
     @staticmethod
     def isblank(string: (str | None)):
         return string is None or string.strip() == ''
+
+
+class Thread(threading.Thread):
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs=None):
+        self._target = target
+        self._args = args
+        if kwargs is None:
+            kwargs = {}
+        self._kwargs = kwargs
+        self._return = None
+        super().__init__(group, target, name, args, kwargs)
+        self._alive = True
+
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args, **self._kwargs)
+            if inspect.iscoroutinefunction(self._target):
+                self._return = asyncio.run(self._return)
+        self._alive = False
+
+    def join(self, **kwargs) -> any:
+        super().join(**kwargs)
+        return self._return
+
+    def is_alive(self) -> bool:
+        return self._alive
+
+
+async def run_on_thread(fn, *args):
+    t = Thread(target=fn, args=args)
+    t.start()
+    while t.is_alive():
+        await asyncio.sleep(0.5)
+    return t.join()
